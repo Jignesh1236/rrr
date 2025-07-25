2e532c307356a2e6a6122d74612be10400723c90
Jignesh1236 <jigneshmaru690@gmail.com>
2025-07-25 13:47:46 +0530

    Delete app directory

--- app/tools/pdf-to-photo/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-export const dynamic = "force-dynamic";
-import dynamicImport from "next/dynamic";
-
-const PDFToPhotoClient = dynamicImport(() => import("./PDFToPhotoClient"), { ssr: false });
-
-export default function PDFToPhotoPage() {
-  return <PDFToPhotoClient />;
-} 
